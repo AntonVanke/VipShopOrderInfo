51834e9af7f4d5d702bab0d62bb79c42be7d4968
AntonVanke <antonvanke@hotmail.com>
2023-05-24 13:47:59 +0800

    details测试

--- main.py
+++ main.py
@@ -71,17 +71,70 @@ def update_orders():
     return orders.all()
 
 
+def update_details():
+    for user in users.search(where("status") == True):
+        orders_sn = [item['order_id'] for item in orders.search(where("user_id") == user["uid"])]
+        check_sn = [item['order_sn'] for item in details.all()]
+        for osn in orders_sn:
+            # Fixme: 减少请求次数
+            if orders_sn in check_sn:
+                if time.time() - details.search(where("order_sn") == orders_sn)[0]["order_time"] > 604800:
+                    continue
+            data = VipShopUser(uid=user["uid"], token=user["token"]).get_details(osn)
+            if data["code"] == 200:
+                # 订单编号
+                order_sn = data["result"]["orderSn"]
+                # 订单时间
+                order_time = data["result"]["orderTime"]
+                # 订单状态
+                order_state = data["result"]["orderTrack"]["result"]["currentState"]
+                # 用户ID
+                user_id = data["result"]["userId"]
+                # 订单商品类数
+                goods_count = data["result"]["goodsList"]["result"]["goodsTotalCount"]
+                # 订单价格
+                total_amount = data["result"]["goodsList"]["result"]["totalAmount"]
+                # 物流单号
+                tracking_sn = data["result"]["orderTrack"]["result"]["orderTrack"][0]["sn"]
+                # 物流信息
+                tracking_message = data["result"]["orderSummary"]["result"]["message"]
+                # 物流电话
+                tracking_mobile = data["result"]["orderSummary"]["result"]["mobile"]
+                # 物流收件人
+                tracking_consignee = data["result"]["orderSummary"]["result"]["consignee"]
+                # 物流地址
+                tracking_address = data["result"]["orderSummary"]["result"]["areaName"] + \
+                                   data["result"]["orderSummary"]["result"]["detailAddress"]
+                # 爬取时间
+                crawl_time = int(time.time())
+
+                # 如果已存在，只修改状态，否则插入
+                order_target = details.search((where("order_sn") == order_sn))
+                if order_target:
+                    orders.update({"order_state": order_state}, (where("order_sn") == order_sn))
+                else:
+                    details.insert(
+                        {"order_sn": order_sn, "order_time": order_time, "order_state": order_state, "user_id": user_id,
+                         "goods_count": goods_count, "total_amount": total_amount, "tracking_sn": tracking_sn,
+                         "tracking_message": tracking_message, "tracking_mobile": tracking_mobile,
+                         "tracking_consignee": tracking_consignee, "tracking_address": tracking_address,
+                         "crawl_time": crawl_time})
+    return details.all()
+
+
 def update_users():
     for _user in users.all():
         if VipShopUser(uid=_user["uid"], token=_user["token"]).is_visible():
-            users.update({"status": True}, where("uid") == _user["uid"])
+            users.update({"status": True, "update_time": int(time.time())}, where("uid") == _user["uid"])
         else:
-            users.update({"status": False}, where("uid") == _user["uid"])
+            users.update({"status": False, "update_time": int(time.time())}, where("uid") == _user["uid"])
 
     for _user in users.search(where("status") == True):
         user_info = VipShopUser(uid=_user["uid"], token=_user["token"]).get_user_info()["data"]
         users.update(
-            {"mobile": user_info["mobile"], "username": user_info["userName"], "nickname": user_info["nickname"]},
+            {"mobile": user_info["mobile"], "username": user_info["userName"], "nickname": user_info["nickname"],
+             "update_time": int(time.time())
+             },
             where("uid") == _user["uid"])
     account_list.delete(0, tk.END)
 
@@ -101,15 +154,16 @@ def update_orders_excel():
     :return:
     """
     data = update_orders()
+    crawl_time = data[-1]['crawl_time']
     workbook = openpyxl.Workbook()
-    worksheet = workbook.active
-    worksheet.title = f"截至{time.strftime('%Y-%m-%d %H.%M', time.localtime(data[-1]['crawl_time']))}订单数据"
+    orders_sheet = workbook.active
+    orders_sheet.title = f"截至{time.strftime('%Y-%m-%d %H.%M', time.localtime(data[-1]['crawl_time']))}订单数据"
 
     headers = ["商品编号", "商品名称", "商品规格", "商品数量", "商品价格", "订单编号", "用户编号", "订单状态",
                "下单时间", "订单金额", "抓取时间"]
 
     for i, header in enumerate(headers):
-        worksheet.cell(row=1, column=i + 1).value = header
+        orders_sheet.cell(row=1, column=i + 1).value = header
 
     # 写入数据到工作表中，从第二行开始
     for i, item in enumerate(data):
@@ -117,13 +171,35 @@ def update_orders_excel():
         for j, (key, value) in enumerate(item.items()):
             # 写入工作表的单元格中，根据键名的顺序和值的类型
             if key in ["order_time", "crawl_time"]:
-                worksheet.cell(row=i + 2, column=j + 1).value = time.strftime('%Y-%m-%d %H:%M', time.localtime(value))
+                orders_sheet.cell(row=i + 2, column=j + 1).value = time.strftime('%Y-%m-%d %H:%M',
+                                                                                 time.localtime(value))
+            elif key in ["product_price", "order_amount"]:
+                orders_sheet.cell(row=i + 2, column=j + 1).value = float(value)
             else:
-                worksheet.cell(row=i + 2, column=j + 1).value = value
+                orders_sheet.cell(row=i + 2, column=j + 1).value = value
+    # 创建用户表
+    data = users.all()
+    users_sheet = workbook.create_sheet("当前用户状态", 1)
+    headers = ["UID", "TOKEN", "状态", "添加时间", "更新时间", "备注", "电话", "用户名", "用户昵称"]
+    for i, header in enumerate(headers):
+        users_sheet.cell(row=1, column=i + 1).value = header
+
+    for i, item in enumerate(data):
+        # 遍历每个字典的键和值
+        for j, (key, value) in enumerate(item.items()):
+            # 写入工作表的单元格中，根据键名的顺序和值的类型
+            if key in ["update_time", "add_time"]:
+                users_sheet.cell(row=i + 2, column=j + 1).value = time.strftime('%Y-%m-%d %H:%M',
+                                                                                time.localtime(value))
+            elif key == "status":
+                users_sheet.cell(row=i + 2, column=j + 1).value = "有效账号" if value else "无效账号"
+            else:
+                users_sheet.cell(row=i + 2, column=j + 1).value = value
 
     # 保存工作簿到文件中
-    workbook.save(f"orders-{time.strftime('%Y年%m月%d日%H时%M分', time.localtime(data[-1]['crawl_time']))}.xlsx")
-    info.config(text=f"导出文件：orders-{time.strftime('%Y年%m月%d日%H时%M分', time.localtime(data[-1]['crawl_time']))}.xlsx")
+    workbook.save(f"orders-{time.strftime('%Y年%m月%d日%H时%M分', time.localtime(crawl_time))}.xlsx")
+    info.config(
+        text=f"导出文件：orders-{time.strftime('%Y年%m月%d日%H时%M分', time.localtime(crawl_time))}.xlsx")
 
 
 def delete_user():
@@ -162,6 +238,7 @@ def get_qr_code():
     qrcode_tk.config(image=tk_pic)
     qrcode_tk.pack()
     scan_success.pack()
+    # print(update_details())
 
 
 def get_cookie():
